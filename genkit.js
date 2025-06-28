b8313df605c3f40bbc14e1a3d4c73758ecdc0dfe
Drive146 <drivebytalrop@gmail.com>
2025-06-28 12:40:10 +0530

    Delete genkit.js

--- genkit.js
+++
@@ -1,7 +0,0 @@
-import '@/ai/flows/store-user-details.ts';
-import '@/ai/flows/send-confirmation-email-flow.ts';
-import '@/ai/flows/get-available-slots-flow.ts';
-import '@/ai/flows/get-booked-slots-for-day-flow.ts';
-import '@/ai/flows/get-scheduler-settings-flow.ts';
-import '@/ai/flows/update-scheduler-settings-flow.ts';
-
